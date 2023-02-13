4a6a9803d5fce0c02ac32fa99464b2cfb0324646
maicondguerian <maicondguerian@gmail.com>
2023-02-13 19:10:13 -0300

    iniciando projeto

---
+++ src/Components/ProductPage.js
@@ -0,0 +1,15 @@
+import React, { Fragment, useState } from 'react'
+import style from './ProductPage.module.css'
+
+export default function ProductPage() {
+  return (
+    <Fragment>
+        <div className={style.wrapperContainer}>
+         <div>
+            
+         </div>
+        </div>
+    </Fragment>
+
+  )
+}
