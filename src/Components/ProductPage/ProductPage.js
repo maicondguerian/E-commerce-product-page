341ed63184902d77a62f38071cd694c083754412
maicondguerian <maicondguerian@gmail.com>
2023-02-14 22:26:45 -0300

    adicionada função incrementar

--- src/Components/ProductPage/ProductPage.js
+++ src/Components/ProductPage/ProductPage.js
@@ -10,8 +10,17 @@ import { FaMinus, FaPlus, } from "react-icons/fa";
 export default function ProductPage() {
     const cartIcon = <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#FFF" fillRule="nonzero" /></svg>
 
-     const counter = 0;
+     const [counter, setCounter] = useState(0);
 
+     const increment  = () =>{
+        setCounter(counter +1)
+        console.log('oi')
+     }
+
+     const decrement  = () =>{
+        setCounter(counter -1)
+        console.log('oi')
+     }
         return (
             <Fragment>
                 <div className={style.wrapperContainer}>
@@ -50,9 +59,9 @@ export default function ProductPage() {
                             <p className={style.lastPrice}>$250.00</p>
                             <div className={style.ButtomWrapper}>
                                 <div className={style.counterWrapper}>
-                                    <CustomButtom IconCustom={FaMinus} nameButtom='' customStyle={style.custom}   />
+                                    <CustomButtom IconCustom={FaMinus} nameButtom='' customStyle={style.custom} onClick={decrement} />
                                     <span className={style.counterValue}><h3>{counter}</h3></span>
-                                    <CustomButtom IconCustom={FaPlus} nameButtom='' customStyle={style.customRight}  />
+                                    <CustomButtom IconCustom={FaPlus} nameButtom='' customStyle={style.customRight} onClick={increment} />
                                 </div>
                                 <CustomButtom customStyle={style.customButtom} Icon={cartIcon} nameButtom='Add to cart' />
                             </div>
