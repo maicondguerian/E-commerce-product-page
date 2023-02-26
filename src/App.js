d805cf047d80460bf0c1d2a5893a601923c0b8b7
maicondguerian <maicondguerian@gmail.com>
2023-02-26 20:48:16 -0300

    gh-pages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ProductPage from './Components/ProductPage/ProductPage';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <ProductPage/>
-    </div>
+    </main>
   );
 }
 
